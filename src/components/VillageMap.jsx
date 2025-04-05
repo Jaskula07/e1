abe995dde1ffc50c790794de3e60542d3d7e43cb
Jaskula07 <jaskula07@gmail.com>
2025-04-05 04:15:04 +0200

    Update VillageMap.jsx

---
+++ src/components/VillageMap.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function VillageMap() {
+  return (
+    <div className="village-map">
+      <h2>Twoja Cyberpunkowa Wioska</h2>
+      <div className="village">
+        {/* Tu możesz renderować budynki lub grafikę reprezentującą wioskę */}
+        <p>Tu pojawią się budynki w zależności od postępów!</p>
+      </div>
+    </div>
+  )
+}
+
+export default VillageMap
